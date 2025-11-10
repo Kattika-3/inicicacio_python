f7ef0fbe67f599ab9ec3b39654b482a99cf75ed5
Kattika-3 <yekateryna.rud09@iesantpol.cat>
2025-11-10 08:14:46 +0000

    algo

---
+++ main.py
@@ -0,0 +1,65 @@
+print("Hola!,món")
+
+#Això és un comentari
+
+"""
+Això és un bloc de comentari
+Puc escriure a totes les línies
+"""
+
+"""
+TIPUS DE VARIABLES:
+*Nombres: enters(int) i Decimals(float)
+*Text: Cadena de caràcters (str)
+*Booleans: Veritat o fals (bool)
+
+Anem a declarar les variables:
+Variables: en minúscula
+CONSTANTS: en majúscula
+"""
+
+enter_1 = 17 #És una variable entera
+float_1 = 3
+
+string_1 = "Això és un string"
+string_2 = 'Un altre string'
+
+boolea_1 = True
+boolea_2 = False
+
+print("Vull imprimir aquest missatge per pantalla")
+print(string_1)
+print(string_2)
+
+print(type(enter_1))
+print(type(float_1))
+print(type(string_1))
+print(type(boolea_1))
+print(type(print))
+print(random.random)
+
+
+print(f"Això és un enter: {enter_1}, això és una float: {float_1}")
+
+#Operadors aritmètics
+operacio = enter_1-float_1 + 4
+print(f"L'opercaió té com a resultat: {operacio} i es tipus{type(opercaio)}")
+
+operacio_complexa = enter_1**5 + float_1/5 - 3*(enter_1 % 4)
+print(operacio_complexa)
+
+print(1%3,2%3,3%3,4%3,5%3,6%3)
+
+
+enter_2 = input("Introdueix un nombre enter: ")
+
+print(enter_2)
+
+
+# CTRL + ç (comento el bloc de codi)
+# #import random
+# import numpy as np
+# import random
+# y = random.choises()
+
+# # print(y)
